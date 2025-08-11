1513e5c727276af745d1bf784ef25de0c88a673e
satya2221 <satyaghifari12@gmail.com>
2025-08-11 10:36:25 +0700

    feat: try make & use component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,11 @@
+import { Header } from "@/components/header"
+import { Body } from "@/components/body"
+import { Footer } from "@/components/footer"
+
 export default function Page(){
-  return <div>Ini Adalah page.js</div>
+  return <div>
+    <Header />
+    <Body />
+    <Footer />
+  </div> 
 }
---
+++ src/components/body.jsx
@@ -0,0 +1,3 @@
+export const Body = () => {
+    return <div>Ini Sebuah Body yang bisa dilanjutkan</div>
+}
