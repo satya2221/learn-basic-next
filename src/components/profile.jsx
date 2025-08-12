cf4c5b6631c064bd81d01a2e103f49d53666408b
satya2221 <satyaghifari12@gmail.com>
2025-08-12 09:38:29 +0700

    feat: destructurization of component props

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,13 +1,13 @@
 'use client'
 
-export const Profile = (props) => {
+export const Profile = ({name, age, address, gender}) => {
     return(
-        <div className={props.gender === 'f' ? "bg-pink-500": "bg-blue-500"}>
+        <div className={gender === 'f' ? "bg-pink-500": "bg-blue-500"}>
             <div className="text-lime-200">
-                Name : {props.name}
+                Name : {name}
             </div>
-            <div>Age: {props.age}</div>
-            <div>Address: {props.address}</div>
+            <div>Age: {age}</div>
+            <div>Address: {address}</div>
             <button 
                 type="button" 
                 className="bg-indigo-500"
